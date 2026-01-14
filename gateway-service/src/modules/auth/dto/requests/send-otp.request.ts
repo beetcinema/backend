2cbfb6893515265c2222350e8a74306b030890c8
Jophkins <sergebrew@gmail.com>
2026-01-13 19:17:05 -0700

    :hammer: chore: updates send.otp.request w/ new validators

--- gateway-service/src/modules/auth/dto/requests/send-otp.request.ts
+++ gateway-service/src/modules/auth/dto/requests/send-otp.request.ts
@@ -1,7 +1,9 @@
-import { IsEnum, IsString } from 'class-validator'
+import { IsEnum, IsString, Validate } from 'class-validator'
+import { IdentifierValidator } from 'src/shared/validators'
 
 export class SendOtpRequest {
 	@IsString()
+	@Validate(IdentifierValidator)
 	identifier: string
 
 	@IsEnum(['email', 'sms'])
